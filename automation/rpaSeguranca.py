852d9374b6db63634808e07e43edfd51d5834cf7
pedro <pedbcrespo@gmail.com>
2023-11-04 12:45:35 -0300

    ajustes

--- automation/rpaSeguranca.py
+++ automation/rpaSeguranca.py
@@ -6,6 +6,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from database import getStates, getStatesCity
 from unidecode import unidecode
+import functools as ft
+import os
+
+def clear():
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name == 'nt':
+        os.system('cls')
 
 class RpaSecurity:
     def __init__(self):
@@ -20,26 +28,22 @@ class RpaSecurity:
             div_table = self.wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
             presentationRowsXpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[11]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div[1]/div[2]/div'
             presentationDiv = div_table.find_element(By.XPATH, presentationRowsXpath)
-            index = 0
             self.securityInfo = []
             data = None 
             while data != ['Selecionar', 'Linha', 'TO', 'Xambioá', '4', '3', '2']:
                 divs = presentationDiv.find_elements(By.CLASS_NAME, 'row')
                 for div in divs:
                     data = div.text.split()
-                    print(data)
                     if data not in self.securityInfo:
                         self.securityInfo.append(data)
-                    index += 1
-                if index == 5570:
-                    break
+                clear()
+                print('dados lidos:', len(self.securityInfo))
                 self.rollDown(scroolBar)
-            print('Dados coletados: ', len(self.securityInfo))
         except:
             print('ERRO AO BUSCAR DADOS DO POWERBI')
         self.cleanInfo()
         self.fixValues()
-        print(self.securityInfo[0])
+        print('Dados coletados: ', len(self.securityInfo))
         
     def cleanInfo(self):
         self.securityInfo = list(map(lambda val: val[2:], self.securityInfo))
@@ -48,24 +52,31 @@ class RpaSecurity:
         def lastElement(arr):
             index = len(arr) -1
             return None if arr == [] else arr[index]
-        
         def handleValues(val):
             names = list(filter(lambda x: not x.isnumeric(), val))
             numbers = list(filter(lambda x: x not in names, val))
             abbreviation = names.pop(0)
             return {'abbreviation': abbreviation, 'name': ''.join(names), 'rate': int(lastElement(numbers))}
-        
-            
         self.securityInfo = list(map(lambda val: handleValues(val), self.securityInfo))    
-        
-            
         
     def rollDown(self, scroolBar):
         actions = ActionChains(driver)
-        actions.click_and_hold(scroolBar).move_by_offset(0, 70).release().perform()
+        actions.click_and_hold(scroolBar).move_by_offset(0, 2).release().perform()
 
+    def createAvarage(self, state):
+        abbreviation = state['abbreviation']
+        stateList = list(filter(lambda val: val['abbreviation'] == abbreviation, self.securityInfo))
+        avg_rate = round(ft.reduce(lambda a, b: a+b, list(map(lambda val: val['rate'], stateList)))/len(stateList), 2)
+        return avg_rate
+        
     def execute(self, state, city):
-        pass
-
+        cityName = city['name']
+        cityRate = list(filter(lambda data: data['name'] == cityName, self.securityInfo))
+        return self.createAvarage(state) if cityRate == [] else cityRate[0]
 
 rpa = RpaSecurity()
+states = getStates()
+for state in states:
+    cities = getStatesCity(state['abbreviation'])
+    for city in cities:
+        print(city['name'], rpa.execute(state, city))
