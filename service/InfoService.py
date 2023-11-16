ed7916b158bcdf9e3ccf6403a2696aab1e9b6260
pedro <pedbcrespo@gmail.com>
2023-11-16 20:48:10 -0300

    implementando recomendador

--- service/InfoService.py
+++ service/InfoService.py
@@ -8,13 +8,20 @@ from model.InfoLightConsume import InfoLightConsume
 from model.InfoLightPrice import InfoLightPrice
 from model.InfoWaterConsumer import InfoWaterConsumer
 from model.InfoWaterPriceRegion import InfoWaterPriceRegion
+from model.FormAtributes import FormAttributes
 
 from configuration.config import ormDatabase, statisticsFunction
 from typing import List
 
 class InfoService:
-    def getRecomendation(self, formAttributes):
+    def getRecomendation(self, formAttributes: FormAttributes):
         recomendation = []
+        homePrices = self.__gettingHomePrices__(formAttributes.priceRate)
+        citiesWithChooseSize = self.__gettingCitiesBySize__(formAttributes.typeCitySize)
+        coustLivinPrices = []
+        
+        
+        
         return recomendation
     
     def getInfo(self, cityId, infoType):
@@ -28,3 +35,12 @@ class InfoService:
         info.update(self.getInfo(cityId, InfoSecurity))
         info.update(self.getInfo(cityId, InfoSchools))
         return info
+    
+    def __gettingHomePrices__(self, price):
+        valueAround = price * 0.25
+        prices = InfoPrices.query.filter(InfoPrices.avg_price <= price + valueAround)
+        return prices
+    
+    def __gettingCitiesBySize__(self, size):
+        typeSizeCities = InfoGeneral.query.filter(InfoGeneral.population >= size['min'] and InfoGeneral.population <= size['max'])
+        return typeSizeCities
