9a9de4bdee579c18fab7fe3d431e41628b1a7e25
pedro <pedbcrespo@gmail.com>
2024-01-02 18:13:02 -0300

    tentando por outro caminho

--- automation/rpaEmpresas.py
+++ automation/rpaEmpresas.py
@@ -4,29 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from database import getStates, getStatesCity
+from unidecode import unidecode
 
+class RpaEmpresas:
+    def getUrl(self, state, city):
+        abbreviation = (state['abbreviation']).lower()
+        cityName = unidecode(city['name']).lower().replace(' ','-')
+        return f"https://www.econodata.com.br/empresas/{abbreviation}-{cityName}"
+    
+    def execute(self):
+        pass
 
-states = getStates()
-
-driver.get("https://public.tableau.com/app/profile/mapadeempresas/viz/MapadeEmpresasnoBrasil_15877433181480/VisoGeral")
-acceptCookiesId = 'onetrust-accept-btn-handler'
-
-wait = WebDriverWait(driver, 20)
-acceptCookiesButton = wait.until(
-        EC.presence_of_element_located((By.ID, acceptCookiesId))
-)
-acceptCookiesButton.click()
-div1class = '_content_919mw_7'
-div2class = '_container_16i18_1'
-
-div = wait.until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'div2class'))
-)
-
-print(div)
-
-# divRoot = driver.find_element(By.ID, 'root')
-
-
-input('')
-driver.close()
+if __name__ == '__main__':
+    pass
