75a1e158b7a37a82973e89774234fdb7d167b975
pedro <pedbcrespo@gmail.com>
2024-01-01 21:45:00 -0300

    atualizando rpa

--- automation/rpaEmpresas2.py
+++ automation/rpaEmpresas2.py
@@ -10,19 +10,10 @@ class rpaEmpresas2:
         pyautogui.moveTo(x, y)
         pyautogui.click()
         time.sleep(timeWait)
-    
-    def __moveDoubleClick__(self, x, y, timeWait=0):
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-        pyautogui.click()
-        time.sleep(timeWait)
 
 
-
-    def execute(self):
+    def accessSite(self):
         url = "https://public.tableau.com/app/profile/mapadeempresas/viz/MapadeEmpresasnoBrasil_15877433181480/VisoGeral"
-        currentMouseX, currentMouseY = pyautogui.position()
-        print(f"{currentMouseX}, {currentMouseY}")
         # CLICA NO BRAVE
         self.__moveClickAndWait__(698, 1058)
 
@@ -34,6 +25,7 @@ class rpaEmpresas2:
         pyautogui.press('enter')
         time.sleep(8)
 
+    def prepareToSearch(self):
         # CLICA NO "EMPRESAS POR ATIVIDADE..."
         self.__moveClickAndWait__(557, 319, 8)
 
@@ -46,6 +38,55 @@ class rpaEmpresas2:
         # CLICA NO INPUT TEXTO DE BUSCA DE CIDADE
         self.__moveClickAndWait__(34, 622)
 
+    def processToDownload(self, cityName):
+        # CLICA NO BOTAO DE BAIXAR
+        self.__moveClickAndWait__(1739, 273, 3)
+        # CLICA NA OPCAO DE BAIXAR DADOS DE TABELA CRUZADOS
+        self.__moveClickAndWait__(937, 717, 1)
+        # SELECIONA A OPCAO CSV
+        self.__moveClickAndWait__(805, 339)
+
+        # CLICA NA OPCAO BAIXAR
+        self.__moveClickAndWait__(1144, 383, 3)
+
+        # CLILCA NO PATH AONDE SALVARA O ARQUIVO
+        # self.__moveClickAndWait__(466, 51, 1)
+        # pyautogui.write("C:\\Users\\User\\Documents\\Programacao\\projeto-tcc\\csvData\\enterprises")
+        # pyautogui.press('enter')
+
+        # CLICA NO CAMPO TEXTO PARA SALVAR O ARQUIVO
+        self.__moveClickAndWait__(405, 465)
+
+        # APERTA SEQUENCIA DE TECLAS PARA SELECIONAR TODO O TEXTO E ESCREVER O NOME DO ARQUIVO
+        pyautogui.hotkey('ctrl', 'a')
+        pyautogui.write(f"{cityName}-empresas")
+        pyautogui.press('enter')
+        pyautogui.press('home')
+        time.sleep(3)
+
+    def preparationToNewSearch(self, cityName):
+        # CLICA NO SELECT DE MUNICIPIOS
+        self.__moveClickAndWait__(40, 594, 1)
+
+        # ESCREVE O NOME DA CIDADE DENOVO    
+        pyautogui.write(cityName)
+        time.sleep(2)
+
+        # CLICA NO RESULTADO
+        self.__moveClickAndWait__(37, 647, 3)
+
+        # APAGA O NOME
+        self.__moveClickAndWait__(267, 620)
+
+        # CLICA NO INPUT TEXTO DO NOME
+        self.__moveClickAndWait__(45, 623)
+
+    def execute(self):
+        currentMouseX, currentMouseY = pyautogui.position()
+        print(f"{currentMouseX}, {currentMouseY}")
+        self.accessSite()
+        self.prepareToSearch()
+
         for city in self.cities:
             # DIGITA NOME DA CIDADE
             pyautogui.write("")
@@ -53,21 +94,14 @@ class rpaEmpresas2:
             time.sleep(3)
 
             # CLICA NO CHECKBOX RESULTADO
-            self.__moveClickAndWait__(37, 650)
-            
-            # ESPERA ALGO
-            time.sleep(5)
-
-            # DESSELECIONA O CHECKBOX RESULTADO
             self.__moveClickAndWait__(37, 650, 5)
 
-            # APAGA O NOME ESCRITO
-            self.__moveClickAndWait__(264, 619, 2)
-
-            # CLICA NO INPUT TEXTO DE BUSCA DE CIDADE
-            self.__moveClickAndWait__(34, 622)
-
-
+            self.processToDownload(city['name'])
+            self.preparationToNewSearch(city['name'])
+        
+        
+        
+                
 if __name__ == '__main__':
     rpa = rpaEmpresas2()
     rpa.execute()
