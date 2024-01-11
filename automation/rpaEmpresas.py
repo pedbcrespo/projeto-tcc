b6c937c96ce62cc9580d903fcf8eedf7fd0cc523
pedro <pedbcrespo@gmail.com>
2024-01-11 18:23:43 -0300

    atualizando

--- automation/rpaEmpresas.py
+++ automation/rpaEmpresas.py
@@ -16,42 +16,41 @@ class RpaEmpresas:
         enterprises = []
         abbreviation = state['abbreviation']
         cities = getStatesCity(abbreviation)
-        for city in cities[:1]:
-            enterprises += self.__getEnterprises__(state, city)
+        for city in cities:
+            enterprises.append(self.__getEnterprises__(state, city))
         return enterprises
 
     def __getEnterprises__(self, state, city):
         info = {}
 
-        xpathCloseAdd = '//*[@id="modal-base-backdrop"]/div/div/div/div/div[1]/img'
-        xpathAcceptCookies = '/html/body/div[1]/div/a'
-        # xpathLogInButton = '//*[@id="__nuxt"]/div/nav/div/div[5]/button'
-        # xpathGoogleLoginButton = '//*[@id="container"]/div/div[2]'
+        xpathAmount = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/h1/span[1]'
+        xpathEnterprisesTypeListButton = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/div[3]/div[3]/div[3]/div[2]/span[2]'
+        xpathTypesTable = '//*[@id="empresas-abertas-recentemente-section"]/div[2]/div[2]/div/div[1]/table/tbody'
 
         driver.get(self.getUrl(state, city))
         wait = WebDriverWait(driver, 10)
+        amountTag = wait.until(EC.presence_of_element_located((By.XPATH, xpathAmount)))
+        typeListButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathEnterprisesTypeListButton)))
+        typeListButton.click()
+        typeTable = wait.until(EC.presence_of_element_located((By.XPATH, xpathTypesTable)))
+        info['amount'] = int(amountTag.text.replace('.', ''))
+        print(info['amount'], typeTable) 
+        return info
+    
+    def execute(self):
+
+        xpathCloseAdd = '//*[@id="modal-base-backdrop"]/div/div/div/div/div[1]/img'
+        xpathAcceptCookies = '/html/body/div[1]/div/a'
+
+        driver.get('https://www.econodata.com.br/empresas/')
+        wait = WebDriverWait(driver, 20)
         
         closeButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathCloseAdd)))
         closeButton.click()
         
         acceptCookiesButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathAcceptCookies)))
         acceptCookiesButton.click()
-        
-        # loginButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathLogInButton)))
-        # loginButton.click()
 
-        # googleButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathGoogleLoginButton)))
-        # googleButton.click()
-
-        xpathAmount = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/h1/span[1]'
-        xpathTopEnterprisesTypes = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/div[3]/div[3]/div[1]'
-        
-        amountTag = wait.until(EC.presence_of_element_located((By.XPATH, xpathAmount)))
-        info['amount'] = amountTag.text
-
-        return []
-    
-    def execute(self):
         enterprises = []
         states = getStates()
         for state in states[:1]:
