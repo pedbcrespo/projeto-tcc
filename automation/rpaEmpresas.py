a58a56c2578c52621b7bcabdf00bac336dca6918
pedro <pedbcrespo@gmail.com>
2024-01-15 14:14:44 -0300

    invertendo metodo prioritario

--- automation/rpaEmpresas.py
+++ automation/rpaEmpresas.py
@@ -1,4 +1,4 @@
-from rpaBase import driver
+from rpaBase import driver, webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -7,53 +7,48 @@ from database import getStates, getStatesCity
 import time
 
 class RpaEmpresas:
-    def getUrl(self, state, city):
-        abbreviation = (state['abbreviation']).lower()
-        cityName = city['name'].lower().replace(' ','-')
-        return f"https://www.econodata.com.br/empresas/{abbreviation}-{cityName}"
-
-    def __getDataFromState__(self, state):
-        enterprises = []
-        abbreviation = state['abbreviation']
-        cities = getStatesCity(abbreviation)
-        for city in cities[:1]:
-            enterprises.append(self.__getEnterprises__(state, city))
-        return enterprises
+    def __init__(self):
+        self.xpath = {
+            'button': '//*[@id="onetrust-accept-btn-handler"]',
+            'iframe' : '//*[@id="embedded-viz-wrapper"]/iframe',
+            'iframeSpan' : '//*[@id="tableauTabbedNavigation_tab_2"]',
+            'MEISelect' : '//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_9"]/div/div[3]/span/div[1]',
+            'MEIALLInput' : '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:opcao_mei:nk5201907744645360639_15609509314568364903_(All)"]/div[2]/input',
+            'NOT_MEIInput' : '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:opcao_mei:nk5201907744645360639_15609509314568364903_0"]/div[2]/input',
+            'citySelect' : '//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_5"]/div/div[3]/span/div[1]',
+            'citySelectALL' : '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:nom_municipio:nk5201907744645360639_5969980573013623668_(All)"]/div[2]/input'
+        } 
 
     def __getEnterprises__(self, state, city):
         info = {}
-
-        xpathAmount = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/h1/span[1]'
-        xpathTopTypes = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/div[3]/div[3]/div[1]'
-        driver.get(self.getUrl(state, city))
-        wait = WebDriverWait(driver, 10)
-        amountTag = wait.until(EC.presence_of_element_located((By.XPATH, xpathAmount)))
-        topDiv = driver.find_element(By.XPATH, xpathTopTypes)
-        print(topDiv.text)
-        info['amount'] = int(amountTag.text.replace('.', ''))
         return info
     
     def execute(self):
-
-        xpathCloseAdd = '//*[@id="modal-base-backdrop"]/div/div/div/div/div[1]/img'
-        xpathAcceptCookies = '/html/body/div[1]/div/a'
-
-
-        driver.get('https://www.econodata.com.br/empresas/')
+        driver.get('https://public.tableau.com/app/profile/mapadeempresas/viz/MapadeEmpresasnoBrasil_15877433181480/VisoGeral')
         wait = WebDriverWait(driver, 20)
-        
         try:
-            closeButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathCloseAdd)))
-            closeButton.click()
-            acceptCookiesButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathAcceptCookies)))
-            acceptCookiesButton.click()
-
+            button = wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['button'])))
+            button.click()
         except:
             pass
-        enterprises = []
-        states = getStates()
-        for state in states[:1]:
-            enterprises += self.__getDataFromState__(state)
+        iframe = wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['iframe'])))
+        driver.switch_to.frame(iframe)
+        span = wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['iframeSpan'])))
+        span.click()
+
+        wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['MEISelect']))).click()
+        wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['MEIALLInput']))).click()
+        time.sleep(2)
+
+        notMeiInput = wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['NOT_MEIInput'])))
+        notMeiInput.click()
+        time.sleep(5)
+
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+        wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['citySelect']))).click()
+        wait.until(EC.presence_of_element_located((By.XPATH, self.xpath['citySelectALL']))).click()
+
+
 
 if __name__ == '__main__':
     rpa = RpaEmpresas()
--- automation/rpaEmpresas2.py
+++ automation/rpaEmpresas2.py
@@ -1,4 +1,4 @@
-from rpaBase import driver, webdriver
+from rpaBase import driver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -7,46 +7,53 @@ from database import getStates, getStatesCity
 import time
 
 class RpaEmpresas:
+    def getUrl(self, state, city):
+        abbreviation = (state['abbreviation']).lower()
+        cityName = city['name'].lower().replace(' ','-')
+        return f"https://www.econodata.com.br/empresas/{abbreviation}-{cityName}"
+
+    def __getDataFromState__(self, state):
+        enterprises = []
+        abbreviation = state['abbreviation']
+        cities = getStatesCity(abbreviation)
+        for city in cities[:1]:
+            enterprises.append(self.__getEnterprises__(state, city))
+        return enterprises
+
     def __getEnterprises__(self, state, city):
         info = {}
+
+        xpathAmount = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/h1/span[1]'
+        xpathTopTypes = '//*[@id="__nuxt"]/div/div[1]/div[3]/div[1]/div[3]/div[3]/div[1]'
+        driver.get(self.getUrl(state, city))
+        wait = WebDriverWait(driver, 10)
+        amountTag = wait.until(EC.presence_of_element_located((By.XPATH, xpathAmount)))
+        topDiv = driver.find_element(By.XPATH, xpathTopTypes)
+        print(topDiv.text)
+        info['amount'] = int(amountTag.text.replace('.', ''))
         return info
     
     def execute(self):
-        xpathButton = '//*[@id="onetrust-accept-btn-handler"]'
-        xpathIframe = '//*[@id="embedded-viz-wrapper"]/iframe'
-        xpathIframeSpan = '//*[@id="tableauTabbedNavigation_tab_2"]'
-        xpathMEISelect = '//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_9"]/div/div[3]/span/div[1]'
-        xpathALLInput = '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:opcao_mei:nk5201907744645360639_15609509314568364903_(All)"]/div[2]/input'
-        xpathNOT_MEIInput = '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:opcao_mei:nk5201907744645360639_15609509314568364903_0"]/div[2]/input'
-        xpathCitySelect = '//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_5"]/div/div[3]/span/div[1]'
-        xpathCitySelectALL = '//*[@id="FI_federated.094r6uj0biqiya0zuf7q10pgukt8,none:nom_municipio:nk5201907744645360639_5969980573013623668_(All)"]/div[2]/input'
+
+        xpathCloseAdd = '//*[@id="modal-base-backdrop"]/div/div/div/div/div[1]/img'
+        xpathAcceptCookies = '/html/body/div[1]/div/a'
 
 
-        driver.get('https://public.tableau.com/app/profile/mapadeempresas/viz/MapadeEmpresasnoBrasil_15877433181480/VisoGeral')
+        driver.get('https://www.econodata.com.br/empresas/')
         wait = WebDriverWait(driver, 20)
+        
         try:
-            button = wait.until(EC.presence_of_element_located((By.XPATH, xpathButton)))
-            button.click()
+            closeButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathCloseAdd)))
+            closeButton.click()
+            acceptCookiesButton = wait.until(EC.presence_of_element_located((By.XPATH, xpathAcceptCookies)))
+            acceptCookiesButton.click()
+
         except:
             pass
-        iframe = wait.until(EC.presence_of_element_located((By.XPATH, xpathIframe)))
-        driver.switch_to.frame(iframe)
-        span = wait.until(EC.presence_of_element_located((By.XPATH, xpathIframeSpan)))
-        span.click()
-
-        wait.until(EC.presence_of_element_located((By.XPATH, xpathMEISelect))).click()
-        wait.until(EC.presence_of_element_located((By.XPATH, xpathALLInput))).click()
-        time.sleep(2)
-
-        notMeiInput = wait.until(EC.presence_of_element_located((By.XPATH, xpathNOT_MEIInput)))
-        notMeiInput.click()
-        time.sleep(5)
-
-        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-        wait.until(EC.presence_of_element_located((By.XPATH, xpathCitySelect))).click()
-        wait.until(EC.presence_of_element_located((By.XPATH, xpathCitySelectALL))).click()
-
-
+        enterprises = []
+        states = getStates()
+        for state in states[:1]:
+            enterprises += self.__getDataFromState__(state)
 
 if __name__ == '__main__':
     rpa = RpaEmpresas()
