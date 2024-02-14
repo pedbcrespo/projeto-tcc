f50fe48b78caa1d4024c3cb56a1979b60e31fbf0
pedro <pedbcrespo@gmail.com>
2024-02-13 21:31:20 -0300

    implementando busca de custo de vida

--- automation/rpaCoustLiving.py
+++ automation/rpaCoustLiving.py
@@ -5,9 +5,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from database import getAllCities, getStates, getStatesCity, getStateById
 from unidecode import unidecode
-class RpaCoustLiving:
+import time
 
-    def formatName(name):
+class RpaCoustLiving:
+    def formatName(self, name):
         nameWithoutAccents = unidecode(name)
         formatedName = nameWithoutAccents.lower().replace(' ', '-')
         return formatedName
@@ -17,13 +18,40 @@ class RpaCoustLiving:
         cityFormated = self.formatName(city['name'])
         return f"http://www.custodevida.com.br/{stateLowerCase}/{cityFormated}/"
     
+    def __getValuesFromTable__(self, table):
+        valueList = []
+        tbody = table.find_element(By.TAG_NAME, 'tbody')
+        rows = tbody.find_elements(By.TAG_NAME, 'tr')
+        for row in rows:
+            textValues = row.text.split('R$')
+            valueList.append({
+                textValues[0]: float(textValues[1].replace('.', '').replace(',', '.'))
+            })
+        return valueList
+            
+
+    def __getInfoCity__(self, state, city):
+        url = self.__getUrl__(state, city)
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        section = wait.until(EC.presence_of_element_located((By.TAG_NAME, 'section')))
+        tables = section.find_elements(By.TAG_NAME, 'table')
+        valuesList = []
+        for table in tables:
+            valuesList += self.__getValuesFromTable__(table)
+        return valuesList
+    
     def execute(self):
         states = getStates()
-        for state in states:
+        cities = getStatesCity(states[0]['abbreviation'])
+        valuesList = []
+        for state in states[:3]:
             cities = getStatesCity(state['abbreviation'])
-            for city in cities:
-                driver.get(self.__getUrl__(state, city))
-
+            for city in cities[:5]:
+                try:
+                    valuesList += self.__getInfoCity__(state, city)
+                except:
+                    print(state['abbreviation'], city['name'], 'Dados nao econtrados')
 if __name__ == '__main__':
     rpa = RpaCoustLiving()
     rpa.execute()
