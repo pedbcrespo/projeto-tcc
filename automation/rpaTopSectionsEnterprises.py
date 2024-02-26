a9471f433063d8298f8094aa4c5ca41cd69a4752
pedro <pedbcrespo@gmail.com>
2024-02-26 12:50:05 -0300

    montando rpa

--- automation/rpaTopSectionsEnterprises.py
+++ automation/rpaTopSectionsEnterprises.py
@@ -1,3 +1,4 @@
+import unidecode
 from rpaBase import driver, webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,6 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from database import getAllCities, getStates, getStatesCity, getStateById, saveEnterprises
 
 class RpaTopSectionsEnterprises:
-    
-    def execute(self):
+    def getUrl(self, state, city):
+        stateAbbreviation = state['abbreviation']
+        cityName = city['name'].replace(' ', '-')
+        location = unidecode.unidecode(f"{stateAbbreviation}-{cityName}").lower()
+        return f"https://www.econodata.com.br/empresas/{location}"
+
+    def getTopSectionsEnterprise(self, wait):
         pass
+
+    def execute(self):
+        for state in getStates():
+            for city in getStatesCity(state['abbreviation']):
+                url = self.getUrl(state, city)
+                driver.get(url)
+                self.getTopSectionsEnterprise(WebDriverWait(driver, 10))
+                
