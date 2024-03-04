8a2de55869d92e5e77ab9fb335b28dfbf1b83fa3
pedro <pedbcrespo@gmail.com>
2024-03-04 09:07:03 -0300

    ajustando configuraçao para resolver problemas de cors

--- configuration/config.py
+++ configuration/config.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from configuration import dev_configuration as db
+from flask_cors import CORS
 
 statisticsFunction = db.CITY_EDUCATION_STATISTIC_BASE_URL
 
 conn = "mysql+pymysql://{}:{}@{}/{}".format(db.user, db.password, db.host, db.database)
 
 app = Flask('__name__')
+CORS(app, origins='*')
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
 
 ormDatabase = SQLAlchemy(app)
