dd86f6db0c9b8414346f3c1833c887aa9a493167
pedro <pedbcrespo@gmail.com>
2024-03-05 21:36:16 -0300

    corrigindo problema da recomendacao

--- controller/CityController.py
+++ controller/CityController.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import redirect
+from flask import redirect, request
 from configuration.config import api
 from configuration.dev_configuration import BASE_URL
 from service.CityService import CityService
@@ -37,7 +37,8 @@ class CompleteCityInfo(Resource):
         return city
     
 class Recomendation(Resource):
-    def post(self, formResult):
+    def post(self):
+        formResult = request.json
         cities = infoService.getRecomendation(formResult)
         return cities
     
