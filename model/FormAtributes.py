64b30a49e5c9069b28490df8cd3cc5ed09e7850a
pedro <pedbcrespo@gmail.com>
2024-03-24 19:14:53 -0300

    ajustando modelagem de perguntas

--- model/FormAtributes.py
+++
@@ -1,9 +0,0 @@
-
-class FormAttributes:
-    def __init__(self, priceRate, coustLivingPriceRate, typeCitySize):
-        self.priceRate = priceRate
-        self.coustLivingPriceRate = coustLivingPriceRate
-        self.typeCitySize = typeCitySize
-        
-    def __str__(self):
-        return f"({self.priceRate}, {self.coustLivingPriceRate}, {self.typeCitySize})"
---
+++ model/FormResult.py
@@ -0,0 +1,13 @@
+class FormResult:
+    def __init__(self, formResult):
+        self.title = formResult['title']
+        self.increase = formResult['increase']
+        self.decrease = formResult['decrease']
+        self.answer = formResult['answer']
+        self.hoursLightEstiamte = formResult['hoursLightEstiamte']
+        self.ltWaterConsume = formResult['ltWaterConsume']
+        self.alimentation = formResult['alimentation']
+        self.hygiene = formResult['hygiene']
+        self.transportation = formResult['transportation']
+        self.health = formResult['health']
+        self.recreation = formResult['recreation']
