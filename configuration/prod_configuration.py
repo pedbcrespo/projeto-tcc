abbc25b695231507ffa172a648567338216af7e0
pedro <pedbcrespo@gmail.com>
2024-08-24 16:11:51 -0300

    atualizando

--- configuration/prod_configuration.py
+++ configuration/prod_configuration.py
@@ -1,8 +1,8 @@
-user='pedbcrespo'
-password='pbc1997-01-03'
-host='pedbcrespo.mysql.pythonanywhere-services.com'
+user='pbcrespo'
+password='tcc#2023'
+host='pbcrespo.mysql.pythonanywhere-services.com'
 port=''
-database='tcc_project'
+database='pbcrespo$default'
 
 IBGE_BASE_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades'
 DISTRICTS_API_BASE_URL = 'http://enderecos.metheora.com/api/cidade/'
